ca347957891db37940181bc09c9c570199157b87
MichaelPetro <32774373+MichaelPetro@users.noreply.github.com>
2023-04-24 22:18:03 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+let saveEl = document.getElementById("save-el")
+let countEl=document.getElementById("count")
+console.log(countEl)
+console.log(saveEl)
+
+let counter=0
+
+
+function increment(){
+    counter+=1
+    countEl.innerText=counter
+}
+
+function decrement(){
+    counter-=1
+    countEl.innerText=counter
+}
+
+function save(){
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    // 3. Render the variable in the saveEl using innerText
+    // NB: Make sure to not delete the existing content of the paragraph
+    let countStrg = counter + " - "
+    saveEl.textContent +=countStrg
+    console.log(counter)
+    countEl.textContent=0
+    counter.textContent=0
+}
